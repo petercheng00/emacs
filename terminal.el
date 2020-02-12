eab7be02fccbfd900cd93075b8c3b82f4f4091de
Peter Cheng <petercheng00@gmail.com>
2020-02-12 14:07:34 -0800

    vterm same window

--- terminal.el
+++ terminal.el
@@ -4,4 +4,9 @@
   :ensure t)
 
 (use-package vterm-toggle
-  :ensure t)
+  :ensure t
+  :config
+  (setq vterm-toggle-fullscreen-p nil)
+  (add-to-list 'display-buffer-alist
+               '((lambda(bufname _) (with-current-buffer bufname (equal major-mode 'vterm-mode)))
+                 (display-buffer-reuse-window display-buffer-same-window))))

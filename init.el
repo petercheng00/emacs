de78ab438827e1bf18a6cfd257a932882100ffe9
Peter Cheng <petercheng00@gmail.com>
2023-01-11 21:47:28 -0800

    Ivy -> vertico. Also get rid of straight for stability

--- init.el
+++ init.el
@@ -1,24 +1,7 @@
-; For native-comp
-(setq comp-deferred-compilation t)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
 
-; Set up straight so that we can set up org
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-; Use use-package as a frontend for straight
-(straight-use-package 'use-package)
-(setq straight-use-package-by-default t)
-
-; Set up org so that we can load the rest of the config
-(use-package org)
+(setq use-package-always-ensure t)
 
 (org-babel-load-file "~/emacs/config.org")

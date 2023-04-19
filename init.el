f8666894dcdaef7555fd7ee8819fc2606e2d5ad9
Peter Cheng <petercheng00@gmail.com>
2023-04-19 05:16:38 +0000

    Fix use-package ensure

--- init.el
+++ init.el
@@ -2,6 +2,8 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+
+(require 'use-package)
 (setq use-package-always-ensure t)
 
 (org-babel-load-file "~/emacs/config.org")
